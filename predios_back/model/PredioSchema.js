ca805bfc60e51a7b14570ab59dbe4787b2ea0102
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-11-06 17:46:44 -0300

    feat: add schemas

---
+++ predios_back/model/PredioSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Predio = new mongoose.Schema({
+    nome: { type: String, required: true, unique: true },
+    sigla: { type: String, required: true, unique: true },
+    andares: {
+        type: Number,
+        required: true,
+        min: [1, 'Mínimo um andar'],
+        max: 100
+    },
+    rua: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Rua', 
+        require: true,
+    }
+
+});
+
+module.exports = mongoose.model("Predio", Predio);
---
+++ predios_back/model/RuaSchema.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const Rua = new mongoose.Schema({
+    descricao: { type: String, required: true, unique:true },
+});
+
+module.exports = mongoose.model("Rua", Rua);
