64b82390945e8fde8c28098528c0c41b309b32f9
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-11-06 19:14:18 -0300

    feat: more adjusts to tipo endpoint

--- predios_back/model/TipoSchema.js
+++ predios_back/model/TipoSchema.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
+
 const Tipo = new mongoose.Schema({
-    nome: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    celular: String,
-    senha: String,
-    dataHoraCad: { type: Date, default: Date.now },
+    descricao: { type: String, required: true, unique:true },
 });
+
 module.exports = mongoose.model("tipo", Tipo);
